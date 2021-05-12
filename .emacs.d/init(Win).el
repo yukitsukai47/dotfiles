013dc64e190ed9602c5ed2ab49aaae8654ac2098
yukitsukai47 <hackavenge47@gmail.com>
2021-05-12 14:15:38 +0900

    update

--- .emacs.d/init(Win).el
+++ .emacs.d/init(Win).el
@@ -93,37 +93,13 @@
 (setq auto-save-default nil)
 ; -------------------------
 
-
-; monokai-theme ------------
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+; ----------------
+; monokai-pro-theme
+; ----------------
 (custom-set-variables
- '(custom-safe-themes
-   (quote
-    ("4e839b24f87c529e837535d0a7880f40ac3867b6e3e73a2cf2bb40bab53d4658" default)))
  '(package-selected-packages
-   (quote
-    (ac-php auto-virtualenv elpy undo-tree leaf-keywords hydra auto-complete))))
+   '(monokai-pro-theme undo-tree leaf-keywords hydra elpy auto-virtualenv ac-php)))
 (custom-set-faces
  )
-(load-theme 'monokai t)
+(load-theme 'monokai-pro t)
 ; -------------------------
-
-
-; -Python- elpyのセットアップ(emacsがelpy-modeになるようにするため)-----
- (when (and (require 'python nil t) (require 'elpy nil t))
-   (elpy-enable))
-; -----------------------------------------------------------------
-
-
-; -PHP- ac-phpのセットアップ--------------
-(require 'cl)
-(add-hook 'php-mode-hook
-            '(lambda ()
-               (auto-complete-mode t)
-               (require 'ac-php)
-               (setq ac-sources '(ac-source-php ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-               (yas-global-mode 1)
-               (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
-               (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
-               ))
-; --------------------------------------
