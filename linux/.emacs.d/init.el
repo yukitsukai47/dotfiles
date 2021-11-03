e1031ac33fdbfab15184db9dddd901699c051785
yukitsukai47 <hackavenge47@gmail.com>
2021-11-03 14:07:30 +0900

    update

--- linux/.emacs.d/init.el
+++ linux/.emacs.d/init.el
@@ -100,7 +100,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(jedi company-jedi counsel flycheck elpy)))
+ '(package-selected-packages '(counsel flycheck elpy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -110,8 +110,8 @@
 (package-initialize)
 
 ; elpy
-; Company,highlight-indentation,pyvenv,yasnippet,s
 (elpy-enable)
+;; virtualenv setting
 (setq elpy-rpc-virtualenv-path 'default)
 (setq elpy-rpc-python-command "python3")
 ; company
--- test/elpa/concurrent-0.5.1/concurrent-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from concurrent-0.5.1/concurrent.el  -*- no-byte-compile: t -*-
-(define-package "concurrent" "0.5.1" "Concurrent utility functions for emacs lisp" '((emacs "24.3") (deferred "0.5.0")) :commit "d012a1ab50edcc2c44e3e49006f054dbff47cb6c" :authors '(("SAKURAI Masashi <m.sakurai at kiwanami.net>")) :maintainer '("SAKURAI Masashi <m.sakurai at kiwanami.net>") :keywords '("deferred" "async" "concurrent") :url "https://github.com/kiwanami/emacs-deferred/blob/master/README-concurrent.markdown")
